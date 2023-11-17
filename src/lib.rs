351fea44d5149b2bc5c318b261df636820d591b9
Tejas Sanap <email@whereistejas.xyz>
2023-11-17 13:45:41 +0000

    Move `build_query` to `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 use std::net::{IpAddr, SocketAddr, UdpSocket};
 
-use crate::{constants::QueryType, decoder::Decoder, message::Message};
+use arrayvec::ArrayVec;
+
+use crate::{
+    constants::{QueryClass, QueryType},
+    decoder::Decoder,
+    domain::encode_domain,
+    header::Header,
+    message::Message,
+    query::Query,
+};
 
 mod constants;
 mod domain;
@@ -11,9 +20,43 @@ mod rr;
 
 mod decoder;
 
+pub fn build_query(id: u16, type_: QueryType, domain: &str) -> ArrayVec<u8, 271> {
+    assert!(
+        domain.as_bytes().len() <= 255,
+        "Domain name cannot have more than 255 octets"
+    );
+
+    let mut query = ArrayVec::<u8, 271>::new();
+
+    let mut header = Header::new(id, 1 << 8);
+    header.qd_count += 1;
+    let question = Query {
+        qname: encode_domain(domain),
+        qtype: type_,
+        qclass: QueryClass::IN,
+    };
+
+    query
+        .try_extend_from_slice(header.as_bytes().as_slice())
+        .unwrap();
+    query
+        .try_extend_from_slice(question.as_bytes().as_slice())
+        .unwrap();
+
+    query
+}
+
+#[test]
+fn check_query_hex() {
+    let query = build_query(0x3c5f, QueryType::A, "www.example.com");
+    assert_eq!(
+        "3c5f0100000100000000000003777777076578616d706c6503636f6d0000010001",
+        hex::encode(query),
+    )
+}
 pub fn send_query(domain: &str, name_server: IpAddr) -> Result<Message, ()> {
     let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind to a random UDP address.");
-    let query = query::build_query(rand::random(), QueryType::A, domain);
+    let query = build_query(rand::random(), QueryType::A, domain);
 
     socket
         .send_to(&query, SocketAddr::new(name_server, 53))
@@ -30,4 +73,3 @@ pub fn send_query(domain: &str, name_server: IpAddr) -> Result<Message, ()> {
 
     Message::from_bytes(decoder)
 }
-
--- src/query.rs
+++ src/query.rs
@@ -3,16 +3,15 @@ use arrayvec::ArrayVec;
 use crate::{
     constants::{QueryClass, QueryType},
     decoder::Decoder,
-    header::Header,
     domain::decode_domain,
 };
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) struct Query {
     /// Encoded domain name.
-    qname: ArrayVec<u8, 255>,
-    qtype: QueryType,
-    qclass: QueryClass,
+    pub(crate) qname: ArrayVec<u8, 255>,
+    pub(crate) qtype: QueryType,
+    pub(crate) qclass: QueryClass,
 }
 
 impl Query {
@@ -36,39 +35,4 @@ impl Query {
             qclass: decoder.read_u16().try_into().unwrap(),
         })
     }
-}
-
-pub fn build_query(id: u16, type_: QueryType, domain: &str) -> ArrayVec<u8, 271> {
-    assert!(
-        domain.as_bytes().len() <= 255,
-        "Domain name cannot have more than 255 octets"
-    );
-
-    let mut query = ArrayVec::<u8, 271>::new();
-
-    let mut header = Header::new(id, 1 << 8);
-    header.qd_count += 1;
-    let question = Query {
-        qname: encode_domain(domain),
-        qtype: type_,
-        qclass: QueryClass::IN,
-    };
-
-    query
-        .try_extend_from_slice(header.as_bytes().as_slice())
-        .unwrap();
-    query
-        .try_extend_from_slice(question.as_bytes().as_slice())
-        .unwrap();
-
-    query
-}
-
-#[test]
-fn check_query_hex() {
-    let query = build_query(0x3c5f, QueryType::A, "www.example.com");
-    assert_eq!(
-        "3c5f0100000100000000000003777777076578616d706c6503636f6d0000010001",
-        hex::encode(query),
-    )
 }
