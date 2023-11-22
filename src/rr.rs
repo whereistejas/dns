969184849e13d45832bc30c185510e29cec950b4
Tejas Sanap <email@whereistejas.xyz>
2023-11-22 17:10:38 +0000

    Add `RData` parsing for `CNAME` and `A` records.

--- src/rr.rs
+++ src/rr.rs
@@ -5,43 +5,64 @@ use crate::{
 };
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) struct ResponseRecord {
+pub struct ResponseRecord {
     name: Domain,
     type_: ResponseType,
     class: ResponseClass,
     ttl: u32,
     rd_length: u16,
-    rdata: RData,
+    pub r_data: RData,
 }
 
 impl ResponseRecord {
-    pub(crate) fn from_bytes(decoder: &mut Decoder) -> Result<Self, ()> {
-        let name = Domain::from_bytes(decoder)?;
+    pub(crate) fn from_bytes(decoder: &mut Decoder) -> Self {
+        let name = Domain::from_bytes(decoder);
         let type_ = decoder.read_u16().try_into().unwrap();
         let class = decoder.read_u16().try_into().unwrap();
-        Ok(Self {
+
+        Self {
             name,
             type_,
             class,
             ttl: decoder.read_u32(),
             rd_length: decoder.read_u16(),
-            rdata: RData::from_bytes(type_, class, decoder)?,
-        })
+            r_data: RData::from_bytes(type_, class, decoder),
+        }
     }
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub(crate) enum RData {
-    CNAME,
-    Other,
+pub enum RData {
+    /// the canonical name for an alias
+    CNAME(Domain),
+    /// a host address
+    A([u8; 4]),
+    /// an authoritative name server
+    NS(Domain),
 }
 
 impl RData {
-    fn from_bytes(
-        type_: ResponseType,
-        class: ResponseClass,
-        decoder: &mut Decoder,
-    ) -> Result<Self, ()> {
-        Err(())
+    fn from_bytes(type_: ResponseType, class: ResponseClass, decoder: &mut Decoder) -> Self {
+        match (class, type_) {
+            (ResponseClass::IN, ResponseType::CNAME) => {
+                let domain = Domain::from_bytes(decoder);
+                Self::CNAME(domain)
+            }
+            (ResponseClass::IN, ResponseType::A) => {
+                let ip_addr = [
+                    decoder.pop().unwrap(),
+                    decoder.pop().unwrap(),
+                    decoder.pop().unwrap(),
+                    decoder.pop().unwrap(),
+                ];
+                Self::A(ip_addr)
+            }
+            (ResponseClass::IN, ResponseType::NS) => {
+                let domain = Domain::from_bytes(decoder);
+                Self::NS(domain)
+            }
+            (_, _) => unimplemented!(),
+        }
     }
 }
